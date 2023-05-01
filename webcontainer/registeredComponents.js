7f09e4906ae21bb337712c08a84a1149da10029a
Zach C <zacheryec001@gmail.com>
2023-04-30 22:46:41 -0700

    Adjusted the webcontainer registeredComponents to fix exports

--- webcontainer/registeredComponents.js
+++ webcontainer/registeredComponents.js
@@ -2,7 +2,7 @@ import FourZeroFourSection from './components/404Section/FourZeroFour';
 import AboutHeadSection from './components/AboutHeadSection/AboutHeadSection';
 import AboutHeadImages from './components/AboutHeadImages/AboutHeadImages';
 import Banner from './components/Banner/Banner';
-//import BlogPage from './components/BlogPage/BlogPage';
+import BlogPage from './components/BlogPage/BlogPage';
 // import Companies from './components/Companies/Companies';
 import ContactSection from './components/ContactSection/ContactSection';
 import EducationWorkExperience from './components/EducationAndWork/EducationAndWork';
@@ -12,22 +12,21 @@ import ServiceWrapper from './components/ServiceWrapper/ServiceWrapper';
 import Sm from './components/SocialMedia/SocialMedia';
 import StatsWrapper from './components/StatsWrapper/StatsWrapper';
 import Testimonial from './components/Testimonial/Testimonial';
-//import CtaSection from './components/CtaSection/CtaSection';
+import CtaSection from './components/CtaSection/CtaSection';
 import Skills from './components/Skills/Skills';
 // import faqSection from './components/faqSection/faqSection';
 import MoreArticles from './components/MoreArticles/MoreArticles';
 import ServicesSection from './components/ServicesSection/ServicesSection';
-import TestimonialSection from '../src/components/TestimonialSection/TestimonialSection';
 
 export const registeredComponents = new Map([
   ['Four Zero Four', FourZeroFourSection],
   ['About Head Images', AboutHeadImages],
   ['About Head Section', AboutHeadSection],
   ['Banner', Banner],
-  //['Blog Page', BlogPage],
+  ['Blog Page', BlogPage],
   // ['Companies', Companies],
-  ['ContactSection', ContactSection],
-  //['Call to Action Section', CtaSection],
+  ['Contact Section', ContactSection],
+  ['Call to Action Section', CtaSection],
   ['Education and Work', EducationWorkExperience],
   // ['FAQ', faqSection],
   ['Footer', Footer],
@@ -38,6 +37,5 @@ export const registeredComponents = new Map([
   ['Skills', Skills],
   ['Social Media', Sm],
   ['Stats Section', StatsWrapper],
-  ['Testimonial', Testimonial],
-  ['Testimonial Section', TestimonialSection]
+  ['Testimonial', Testimonial]
 ]);
